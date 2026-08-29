e556265793f33db0b6986aea2ddc93033cce6568
RahulSerain <127072368+RahulSerain@users.noreply.github.com>
2026-08-29 19:50:07 +0530

    Update t test.R

--- Example5_One_Sample_t_test/t test.R
+++ Example5_One_Sample_t_test/t test.R
@@ -1,3 +1,60 @@
+###############################################################
+# Example: Two-sided One-sample Student's t-test
+#
+# Problem:
+#   Sequential large-scale multiple testing for
+#
+#       H0: μ = μ0
+#       H1: μ ∈ {μ1, μ2}
+#
+# This script implements the sequential two-sided one-sample
+# Student's t-test using the Data-driven Intersection (DI)
+# procedure.
+#
+# For each hypothesis, observations are generated from one of
+# the distributions
+#
+#       N(μ0, 1), N(μ1, 1), or N(μ2, 1),
+#
+# according to the mixing proportions specified by p.
+#
+# At each stage, the one-sample Student's t-statistic is
+# calculated and transformed to a z-score using its
+# Student's t distribution. The local false discovery rates
+# are estimated from the observed z-scores using lfdr.gen().
+# The adaptive cutoff is then obtained using adpt.cutz().
+#
+# Sampling continues until the DI procedure produces a valid
+# rejection/acceptance region.
+#
+# Procedure:
+#   Data-driven Intersection (DI) procedure
+#
+# Required code:
+#   lfdr.gen.R.txt
+#   adpt.cutz.R.txt
+#
+# Main function:
+#   one.sample.t.di()
+#
+# Parameters:
+#   m       : Number of hypotheses
+#   p       : Vector of proportions corresponding to
+#             μ0, μ1 and μ2
+#   k       : Initial sample size for each hypothesis
+#   mu0     : Mean under the null hypothesis
+#   mu1     : First alternative mean
+#   mu2     : Second alternative mean
+#   alpha   : Target false discovery rate level
+#
+# Output:
+#   A vector containing:
+#     • Sample size at stopping
+#     • False Discovery Rate (FDR)
+#     • False Non-discovery Rate (FNR)
+#
+###############################################################
+
 
 dat.gen = function ( theta , mu0 , mu1 , mu2 ) {
   
@@ -9,140 +66,120 @@ dat.gen = function ( theta , mu0 , mu1 , mu2 ) {
   
   m <- ncol ( theta )
   
-  X <- theta1 * rnorm ( m , mu0 , 1 ) + theta2 * rnorm ( m , mu1 , 1 ) + theta3 * rnorm (m , mu2 , 1 )
+  X <- theta1 * rnorm ( m , mu0 , 1 ) +
+       theta2 * rnorm ( m , mu1 , 1 ) +
+       theta3 * rnorm ( m , mu2 , 1 )
   
   return ( X )
-
+  
 }
 
 
-z_val <- function ( x , n  ) {
+data.gen.n = function ( theta , mu0 , mu1 , mu2 , n ) {
   
-  x1 <- x [ 1 : n ]
+  replicate ( n , dat.gen ( theta , mu0 , mu1 , mu2 ) )
   
-  x2 <- x [ ( n + 1 ) : length ( x ) ]
-  
-  #t <- ( mean ( x2 ) - mean ( x1 ) ) / (sqrt ( ( ( n1 - 1 ) * ( sd ( x1 ) ) ^ 2 + ( n2 - 1 ) * ( sd ( x2 ) ) ^ 2 ) / ( n1 + n2 - 2 ) )*sqrt(1/n1+1/n2))
-  
-  t.t <- t.test(x1, x2, alternative="two.sided", var.equal=TRUE)
-  
-  z <- qnorm ( pt ( as.numeric ( t.t$statistic ) , as.numeric ( t.t$parameter ) ) )
-  
-  #p <- as.numeric ( t.t$p.value )
-  
-  return ( z )
-
 }
 
 
-
-data.gen.n = function ( theta , mu0 , mu1 , mu2 ,  n) {
-  
-  replicate ( n , dat.gen ( theta , mu0 , mu1 , mu2 ) ) 
-  
-  }
-
-  
 data.gen.x = function ( theta , mu0 , mu1 , mu2 , X ) {
   
-  u <-  dat.gen ( theta , mu0 , mu1 , mu2 ) 
+  u <- dat.gen ( theta , mu0 , mu1 , mu2 )
   
   cbind ( X , u )
   
 }
 
-p.func <- function ( t , n ) {
+
+t_val = function ( x , mu0 ) {
   
-  p <- 2 * min ( 1 - pt (  t  , n-2) , pt ( t , ( n - 2 ) ) )
+  n <- length ( x )
+  
+  t <- ( mean ( x ) - mu0 ) /
+       ( sd ( x ) / sqrt ( n ) )
+  
+  return ( t )
+  
+}
+
+
+t.val.x = function ( X , mu0 ) {
+  
+  t.v <- function ( x ) {
     
-  return(p)
+    t_val ( x , mu0 )
+    
+  }
+  
+  apply ( X , 1 , t.v )
   
 }
 
-z.func <- function ( t , n ) {
+
+z.func = function ( t , n ) {
   
-  qnorm ( pt ( t , ( n - 2 ) ) )
+  z <- qnorm ( pt ( t , n - 1 ) )
+  
+  return ( z )
   
 }
 
 
-two.side.t <- function (m , p , k , mu0 , mu1 , mu2 , alpha , beta ) {
+one.sample.t.di = function ( m , p , k , mu0 , mu1 , mu2 ,
+                            alpha ) {
   
   theta <- rmultinom ( m , 1 , p )
-
-  n <- 2 * k
   
-  X1 <- replicate ( k,rnorm (m,0,1))
-    
-  X2 <- data.gen.n ( theta , mu0 , mu1 , mu2 , k) 
+  X <- data.gen.n ( theta , mu0 , mu1 , mu2 , k )
   
-  X <- cbind ( X1 , X2 )
+  t <- t.val.x ( X , mu0 )
   
-  t.v <- function(x) z_val( x , n )
+  z1 <- z.func ( t , k )
   
-  z1 <- apply ( X , 1 , t.v )
-
   lfdr1 <- lfdr.gen ( z1 )
-
-  lf <- LfdrI ( lfdr1 , alpha , beta )
-
-  del <- lf $ cutoff [ 1 ] - lf $ cutoff [ 2 ]
-
+  
+  adpz <- adpt.cutz ( lfdr1 , alpha )
+  
+  del <- adpz$cutoff[1] - adpz$cutoff[2]
+  
+  
   while ( del < 0 ) {
-  
-    k = k + 1
-  
-    n <- 2 * k
     
-    X1 <- cbind ( X1 , rnorm ( m , 0 , 1 ) )
+    k <- k + 1
     
-    X2 <- data.gen.x ( theta , mu0 , mu1 , mu2 , X2 ) 
-  
-    X <- cbind ( X1 , X2 )
+    X <- data.gen.x ( theta , mu0 , mu1 , mu2 , X )
     
-    t.v <- function(x) z_val( x , n )
+    t <- t.val.x ( X , mu0 )
     
-    z1 <- apply ( X , 1 , t.v )
-  
+    z1 <- z.func ( t , k )
+    
     lfdr1 <- lfdr.gen ( z1 )
-  
-    lf <- LfdrI ( lfdr1 , alpha , beta )
-  
-    del <- lf $ cutoff [ 1 ] - lf $ cutoff [ 2 ] 
     
-    #print(c ( n , del , jin.cai.pi0(z1,0,1)))
-  }
-
-  theta1 <- 1 - theta [ 1 , ]
-
-  D1 = rep ( 0 , m )
-  
-  if ( is.na(length ( lf $ rej.hypo )) == 0  ) {
+    adpz <- adpt.cutz ( lfdr1 , alpha )
     
-    D1 [ lf $ rej.hypo ] <-  rep ( 1 , length ( lf $ rej.hypo ) ) 
+    del <- adpz$cutoff[1] - adpz$cutoff[2]
     
   }
-
-  fr1 <- sum ( D1 > theta1) / max (sum ( D1 ) , 1 )
-
-  fn1 <- sum (D1 < theta1) / max ( sum ( 1 - D1 ), 1 )
-
-  D2 = rep ( 1 , m )
   
-  if ( is.na(length ( lf $ acc.hypo )) == 0 ) {
+  
+  theta1 <- 1 - theta[1,]
+  
+  D <- rep ( 0 , m )
+  
+  if ( length ( adpz$re ) > 0 ) {
     
-    D2 [ lf $ acc.hypo ] <-  rep ( 0 , length ( lf $ acc.hypo ) )
+    D[adpz$re] <- rep ( 1 , length ( adpz$re ) )
     
   }
-
-  fr2 <- sum ( D2 > theta1) / max (sum ( D2 ) , 1 )
-
-  fn2 <- sum (D2 < theta1) / max ( sum ( 1 - D2 ), 1 )
-
- c( n , fr1 , fn1  , fr2 , fn2 ) 
-
+  
+  
+  fdr <- sum ( D > theta1 ) /
+         max ( sum ( D ) , 1 )
+  
+  fnr <- sum ( D < theta1 ) /
+         max ( sum ( 1 - D ) , 1 )
+  
+  
+  c ( k , fdr , fnr )
+  
 }
-
-
-
-
